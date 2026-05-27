d57b8fdddcbd2a66746e1a64be6e01d33950ef46
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-05-27 11:24:35 -0300

    chore: Apply minor formatting and remove blank lines

--- portflow/terminal-service/src/main/java/br/com/edu/infnet/application/services/TerminalService.java
+++ portflow/terminal-service/src/main/java/br/com/edu/infnet/application/services/TerminalService.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class TerminalService {
+
   private final TerminalRepository terminalRepository;
 
   public List<Terminal> findAll() {
