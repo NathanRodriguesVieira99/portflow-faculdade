67c5479f66f51d6cb271d6804fa075970ecab6fe
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2026-06-02 21:14:33 -0300

    feat(kafka): Implement Kafka service for pending documentation events

---
+++ portflow/container-service/src/main/java/br/com/edu/infnet/infrastructure/kafka/KafkaService.java
@@ -0,0 +1,22 @@
+package br.com.edu.infnet.infrastructure.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import br.com.edu.infnet.presentation.events.ContainerStatusEvent;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaService {
+  private final KafkaTemplate<String, ContainerStatusEvent> kafka;
+
+  private void sendEvent(ContainerStatusEvent event) {
+    kafka.send("portflow.container.documentacao_pendente", event.containerId(), event);
+  }
+
+  public void sendDocumentacaoPendente(String containerId) {
+    ContainerStatusEvent containerStatusEvent = ContainerStatusEvent.documentacaoPendente(containerId);
+    sendEvent(containerStatusEvent);
+  }
+}
